92950e193f97ccee2666377a4bdc5b3f6ee42ea7
Indu2003-09 <pranathi.rfc8@gmail.com>
2024-05-27 14:51:54 +0530

    G1

---
+++ cypress/e2e/API/methods.cy.js
@@ -0,0 +1,23 @@
+///  <reference types='Cypress' />
+import resource from '../../support/resource';
+//const resource = require '../../support/resource';
+
+describe("example", () => {
+  it("get method", () => {
+    cy.request("GET", resource.API_BASE_PATH+ `api/users?page=2`).then((response) => {
+      expect(response.status).to.eq(200);
+     // expect(response.body.data[0].id).to.eq(7);
+    });
+    cy.request({
+        method: 'GET',
+        url: `https://reqres.in/api/users`,
+        qs: {
+          page: 2,
+        },
+      }).then(response => {
+        expect(response.status).to.equal(200);
+        expect(response.body).to.not.be.empty;
+      });
+  });
+  
+});
--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -19,5 +19,4 @@ import './commands'
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
 
- //import 'cypress-mochawesome-reporter/register';
-// import 'mochawesome/register';
+
